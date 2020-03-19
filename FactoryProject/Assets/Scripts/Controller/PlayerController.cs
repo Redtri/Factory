c2777fecfdfa1c5b2169a2af18f677e40b253aa8
Redtri <redtri@outlook.com>
2020-03-19 17:10:17 +0100

    [ADD] FPS Controller

---
+++ FactoryProject/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInput playerInput;
+    [HideInInspector] public Vector2 movementInput;
+    [HideInInspector] public Vector2 lookInput;
+
+    [HideInInspector] public bool jumpInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnEnable()
+    {
+        //Movement input listener
+        playerInput.currentActionMap["Movement"].performed += OnMovement;
+        playerInput.currentActionMap["Movement"].canceled += OnMovement;
+        //Look input listener
+        playerInput.currentActionMap["Look"].performed += OnLook;
+        playerInput.currentActionMap["Look"].canceled += OnLook;
+        
+        //playerInput.currentActionMap["OnLook"].performed += OnJump;
+    }
+
+    private void OnMovement(InputAction.CallbackContext value)
+    {
+        movementInput = value.ReadValue<Vector2>();
+    }
+
+    private void OnLook(InputAction.CallbackContext value)
+    {
+        lookInput = value.ReadValue<Vector2>();
+    }
+
+    private void OnJump(InputAction.CallbackContext value)
+    {
+        jumpInput = value.started;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ FactoryProject/Assets/Scripts/Pawns/Pawn.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour
+{
+    [Header("PARAMETERS")]
+    public float lookSensitivity;
+    public Vector2 clampLook;
+
+    public float moveSpeed;
+    
+    [Header("COMPONENTS")]
+    public PlayerController controller;
+    public CharacterController characterController;
+    public Camera cam;
+
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Look();
+        Move();
+    }
+
+    private void Move()
+    {
+        float xMove = controller.movementInput.x;
+        float yMove = controller.movementInput.y;
+        
+        Vector3 movement = transform.right * xMove + transform.forward * yMove;
+        movement *= moveSpeed * Time.deltaTime;
+        
+        characterController.Move(movement);
+    }
+
+    private void Look()
+    {
+        float xLook = controller.lookInput.x * lookSensitivity * Time.deltaTime;
+        float yLook = controller.lookInput.y * lookSensitivity * Time.deltaTime;
+
+        xRotation -= yLook;
+        xRotation = Mathf.Clamp(xRotation, clampLook.x, clampLook.y);
+        
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * xLook);
+    }
+}
