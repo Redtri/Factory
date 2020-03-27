e972536ee07d65d29938dc800706a117c6c9e609
Guillaume <red.tri@outlook.com>
2020-03-27 04:21:23 +0100

    [ADD] First materialization shader iteration

---
+++ FactoryProject/Assets/Scripts/Actors/AppearRandom.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AppearRandom : MonoBehaviour
+{
+    public float distance;
+    
+    public Vector2 durationRange;
+    private Vector3 finalRotation;
+    private Vector3 startRotation;
+    private Vector3 finalPosition;
+    private Vector3 startPosition;
+
+    private static float duration;
+    private float startTime;
+    private MeshRenderer mesh;
+
+    private bool appeared;
+
+    private bool appearing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        TryGetComponent(out mesh);
+        appeared = false;
+        appearing = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Appear();
+        }
+        if (appearing)
+        {
+            if (Time.time - startTime < duration)
+            {
+                float percent = (Time.time - startTime) / duration;
+
+                if (mesh)
+                {
+                    mesh.material.SetFloat("Percent", 1f-(percent));
+                }
+                
+                transform.position = Vector3.Lerp(startPosition, finalPosition, percent);
+                transform.rotation = Quaternion.Lerp(Quaternion.Euler(startRotation), Quaternion.Euler(finalRotation), percent);
+            }
+            else
+            {
+                appeared = true;
+                appearing = false;
+                transform.position = finalPosition;
+                transform.rotation = Quaternion.Euler(finalRotation);
+            }
+        }
+    }
+
+    public void Appear()
+    {
+        if (!appearing && !appeared)
+        {
+            print("appear");
+            duration = Random.Range(durationRange.x, durationRange.y);
+            finalPosition = transform.position;
+            finalRotation = transform.rotation.eulerAngles;
+            
+            transform.position += Random.insideUnitSphere * distance;
+            transform.rotation = Quaternion.Euler(new Vector3(Random.Range(0f, 360f),Random.Range(0f, 360f),Random.Range(0f, 360f) ));
+
+            startPosition = transform.position;
+            startRotation = transform.rotation.eulerAngles;
+            
+            appearing = true;
+            startTime = Time.time;
+        }
+    }
+}
---
+++ FactoryProject/Assets/Scripts/Actors/PortalScreen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScreen : MonoBehaviour
+{
+    public MeshRenderer renderer;
+    private void OnTriggerEnter(Collider other)
+    {
+        renderer.enabled = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        renderer.enabled = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
