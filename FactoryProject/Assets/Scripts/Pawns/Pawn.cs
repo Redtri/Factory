5744ca9edfd64867adc105dab65c87c9a6dfdb2e
Guillaume <red.tri@outlook.com>
2020-04-01 00:27:55 +0200

    [UPDATE] Wall collision fixing

--- FactoryProject/Assets/Scripts/Pawns/Pawn.cs
+++ FactoryProject/Assets/Scripts/Pawns/Pawn.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+using System.Linq;
 
 public class Pawn : MonoBehaviour
 {
@@ -53,6 +55,7 @@ public class Pawn : MonoBehaviour
         Pick();
     }
 
+    private bool switched;
     private void Move()
     {
         float xMove = controller.movementInput.x;
@@ -60,6 +63,37 @@ public class Pawn : MonoBehaviour
         
         Vector3 movement = transform.right * xMove + transform.forward * yMove;
         movement *= moveSpeed * Time.deltaTime;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit,2f))
+        {
+            MeshCollider meshCollider = hit.collider as MeshCollider;
+            if (!meshCollider || !meshCollider.sharedMesh)
+                return;
+
+            Mesh mesh = meshCollider.sharedMesh;
+            Vector3[] vertices = mesh.vertices;
+            int[] triangles = mesh.triangles;
+            Vector3 p0 = vertices[triangles[hit.triangleIndex * 3 + 0]];
+            Vector3 p1 = vertices[triangles[hit.triangleIndex * 3 + 1]];
+            Vector3 p2 = vertices[triangles[hit.triangleIndex * 3 + 2]];
+            Transform hitTransform = hit.collider.transform;
+            p0 = hitTransform.TransformPoint(p0);
+            p1 = hitTransform.TransformPoint(p1);
+            p2 = hitTransform.TransformPoint(p2);
+            Debug.DrawLine(p0, p1, Color.red);
+            Debug.DrawLine(p1, p2, Color.red);
+            Debug.DrawLine(p2, p0, Color.red);
+
+            Vector3 forward = hit.normal;
+            Vector3 center = new Vector3((p0.x + p1.x + p2.x)/3, (p0.y + p1.y + p2.y)/3, (p0.z + p1.z + p2.z)/3);
+            
+            Debug.DrawLine(center, center + forward * 10f, Color.cyan);
+                //transform.rotation = Quaternion.FromToRotation(Vector3.up, -forward);
+            //transform.Rotate(forward);
+            //Debug.DrawLine(hit.collider.transform.position, hit.collider.transform.position + hit.collider.transform.forward * 10f);
+        }
         
         characterController.Move(movement);
     }
